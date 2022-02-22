1d724fdf1d839094f6353d9322ec9b461428cb05
zytuu <96903349+zytuu@users.noreply.github.com>
2022-02-23 00:51:33 +1300

    landing page ig
    bored and made this

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementsByClassName("nav__toggle")[0];
+const navLinks = document.getElementsByClassName("nav__item");
+
+toggleButton.addEventListener("click", function () {
+  for (var i = 0; i < navLinks.length; i++)
+    navLinks[i].classList.toggle("expand");
+});
